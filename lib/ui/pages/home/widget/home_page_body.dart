2c0822ccc777277e2040915adb29225153549310
saki8661 <saki8661@naver.com>
2024-01-11 18:32:35 +0900

    메인 스크린 다가오는 캠핑 데이터 바인딩 성공

--- lib/ui/pages/home/widget/home_page_body.dart
+++ lib/ui/pages/home/widget/home_page_body.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:team_project/_core/constants/color.dart';
 import 'package:team_project/_core/constants/move.dart';
 import 'package:team_project/_core/constants/size.dart';
 import 'package:team_project/ui/pages/home/widget/home_page_click_button.dart';
+import 'package:team_project/ui/pages/my_camping_schedule/my_camping_schedule_view_model.dart';
 
-import '../../../../_core/constants/color.dart';
 import 'explain_bar_form.dart';
 import 'home_carousel_slider.dart';
 
-class HomePageBody extends StatelessWidget{
-  const HomePageBody({
-    super.key,
-  });
+class HomePageBody extends ConsumerWidget {
+  const HomePageBody({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // 홈 화면에 처음 접근할 때 데이터 로드
+    ref.read(myCampingScheduleProvider.notifier).notifyInit();
+
+    // MyCampingScheduleViewModel을 통해 데이터 가져오기
+    final myCampingScheduleModel = ref.watch(myCampingScheduleProvider);
+
+    // 0번지의 데이터 가져오기
+    final campingDataAtIndex0 =
+        myCampingScheduleModel?.campingScheduleList?.isNotEmpty == true
+            ? myCampingScheduleModel!.campingScheduleList![0]
+            : null;
+
     return Container(
       color: kSubColor,
       child: Padding(
@@ -23,12 +35,21 @@ class HomePageBody extends StatelessWidget{
             HomepageClickButton(),
             HomeCarouselSlider(),
             SizedBox(height: gapLarge),
-            ExplainBarForm(
-              symbol: "D-5",
-              title: "다가오는 캠핑",
-              subTitle: "01/09(화) 대저생태공원 캠핑장",
-              pageAddress: Move.myCampingSchedulePage,
-            ),
+            if (campingDataAtIndex0 != null) ...[
+              ExplainBarForm(
+                symbol: campingDataAtIndex0.checkInDDay ?? '',
+                title: "다가오는 캠핑",
+                subTitle:
+                    "${campingDataAtIndex0.checkInDate} ${campingDataAtIndex0.campField ?? '알 수 없는 캠핑장'}",
+                pageAddress: Move.myCampingSchedulePage,
+              ),
+            ] else ...[
+              ExplainBarForm(
+                  symbol: "#",
+                  title: "다가오는 캠핑이 없습니다",
+                  subTitle: "캠핑을 예약해보세요",
+                  pageAddress: Move.myCampingSchedulePage), // 필요에 따라 메시지를 조절하세요
+            ],
             SizedBox(height: gapMain),
             ExplainBarForm(
               symbol: "#",
@@ -42,4 +63,3 @@ class HomePageBody extends StatelessWidget{
     );
   }
 }
-
