e2dcdffb49797863f03860e614e9998b53d276c1
unyak <0106un@naver.com>
2024-01-16 12:34:53 +0900

    필터링 구현 완료

--- lib/ui/pages/my_page/my_page_like_page/my_page_like_page_widgets/my_page_like_list_page.dart
+++ lib/ui/pages/my_page/my_page_like_page/my_page_like_page_widgets/my_page_like_list_page.dart
@@ -24,48 +24,64 @@ class LikeListPage extends ConsumerWidget {
         SliverList(
           delegate: SliverChildBuilderDelegate(
                 (context, index) {
-                  CampBookmarkDTO campBookmarkList = model!.campBookmarkList![index];
-              return InkWell(
-                child: ListTile(
-                  leading: ClipRRect(
-                    child: Image.network("http://192.168.0.134:8080${campBookmarkList.campImage}", width: 65, height: 65, fit: BoxFit.cover,),
-                    borderRadius: BorderRadius.circular(gapMedium),
-                  ),
-                  title: Text(
-                    "${campBookmarkList.campName}",
-                    style: TextStyle(
-                        fontSize: fontSemiMedium,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "${campBookmarkList.campAddress}",
-                        style: TextStyle(
-                            fontSize: fontMedium, color: kFontContent),
+               if(model.campBookmarkList!.isEmpty) {
+                return Column(
+                  children: [
+                    SizedBox(height: gapXxLarge),
+                    SizedBox(height: gapXxLarge),
+                    Container(
+                      height: getScreenWidth(context) * 1.0,
+                      width: getScreenWidth(context) * 1.0,
+                      child: imageLikePage(),
+                    ),
+                  ],
+                );
+                  }
+                  else {
+                 CampBookmarkDTO campBookmarkList = model!.campBookmarkList![index];
+                 return InkWell(
+                      child: ListTile(
+                        leading: ClipRRect(
+                          child: Image.network("http://192.168.0.134:8080${campBookmarkList.campImage}", width: 65, height: 65, fit: BoxFit.cover,),
+                          borderRadius: BorderRadius.circular(gapMedium),
+                        ),
+                        title: Text(
+                          "${campBookmarkList.campName}",
+                          style: TextStyle(
+                              fontSize: fontSemiMedium,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "${campBookmarkList.campAddress}",
+                              style: TextStyle(
+                                  fontSize: fontMedium, color: kFontContent),
+                            ),
+                            Row(
+                              children: [
+                                iconFullStar(
+                                    mColor: kReviewColor, mSize: gapSemiMedium),
+                                Text(
+                                  "${campBookmarkList.campRating}",
+                                  style: TextStyle(
+                                      fontSize: fontSemiMedium,
+                                      color: kFontContent),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
                       ),
-                      Row(
-                        children: [
-                          iconFullStar(
-                              mColor: kReviewColor, mSize: gapSemiMedium),
-                          Text(
-                            "${campBookmarkList.campRating}",
-                            style: TextStyle(
-                                fontSize: fontSemiMedium,
-                                color: kFontContent),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-                onTap: () {
-                  Navigator.pushNamed(context, Move.campsiteDetailPage);
-                },
-              );
+                      onTap: () {
+                        Navigator.pushNamed(context, Move.campsiteDetailPage);
+                      },
+                    );
+                  }
+
             },
-            childCount: model!.campBookmarkList!.length,
+            childCount: model.campBookmarkList!.isEmpty ? 1 : model!.campBookmarkList!.length,
           ),
         )
       ],
