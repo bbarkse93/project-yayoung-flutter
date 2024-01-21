534fe858bd0b5af2ec6296add4dd52e833ecbfb1
yak <0106un@naver.com>
2024-01-21 12:38:39 +0900

    에러 수정 1차

--- lib/_core/constants/http.dart
+++ lib/_core/constants/http.dart
@@ -3,7 +3,6 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 final Dio dio = Dio(
   BaseOptions(
-
     // baseUrl: "http://192.168.0.141:8080", // 메인 서버
     // baseUrl: "http://192.168.0.43:8080", // 메인 서버
     // baseUrl: "http://192.168.0.43:8080", // 세환 서버
@@ -11,8 +10,7 @@ final Dio dio = Dio(
     // baseUrl: "http://192.168.0.41:8080", // 은혜 서버
     // baseUrl: "http://192.168.0.48:8080", // 승신 서버
     // baseUrl: "http://192.168.0.167:8080",  // 대욱 서버
-     baseUrl: "http://192.168.0.134:8080", // 언약 서버
-
+    baseUrl: "http://192.168.33.3:8080", // 언약 서버
 
     contentType: "application/json; charset=utf-8",
   ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
-import 'package:team_project/_core/constants/http.dart';
 import 'package:team_project/_core/constants/move.dart';
 import 'package:team_project/_core/constants/theme.dart';
 
 GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 // TODO: Test위해서 토큰 가지게 셋팅
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  String token =
-      "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJwcm9qZWN0LWtleSIsImlkIjoxLCJ1c2VybmFtZSI6bnVsbCwiZXhwIjo0ODU5MDUzNDgyfQ.Ky2-BLYTjxlouBRsY1HScpc3fC3FOhpK0OrCKy3MFFW6KkCC19B2KsZrd9NIYLoeYY1YEB2BQNLT_KjPETTPMw";
-  await secureStorage.write(key: "jwt", value: token);
+  // String token =
+  //     "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJwcm9qZWN0LWtleSIsImlkIjoxLCJ1c2VybmFtZSI6bnVsbCwiZXhwIjo0ODU5MDUzNDgyfQ.Ky2-BLYTjxlouBRsY1HScpc3fC3FOhpK0OrCKy3MFFW6KkCC19B2KsZrd9NIYLoeYY1YEB2BQNLT_KjPETTPMw";
+  // await secureStorage.write(key: "jwt", value: token);
   KakaoSdk.init(nativeAppKey: "edc929f8703514e3d119422f3935375b");
-
 
   runApp(
     ProviderScope(
       child: const MyApp(),
-
     ),
   );
-
 }
 
 class MyApp extends StatelessWidget {
